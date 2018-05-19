825a5f1fdd9b3ee38d6fdded0e4e2ef40443bb98
Graham Plata <graham.plata@gmail.com>
2018-05-19 16:25:28 -0400

    remove css

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
-import "./Card.css";
+
+const styles = {
+  card: {
+    maxWidth: "350px",
+    borderWidth: "1px",
+    borderStyle: "solid",
+    alignItems: "stretch",
+    justifyContent: "left",
+    backgroundColor: "#ecf0f1"
+  },
+  image: {
+    flexGrow: 1,
+    height: null,
+    width: "100%",
+    opacity: "40%",
+    background: ["linear-gradient(90deg, black, #111)", "black"]
+  },
+  spanTitle: {
+    margin: 24,
+    fontSize: 48,
+    fontWeight: "bold",
+    backgroundColor: "transparent"
+  }
+};
 
 class Card extends Component {
   render() {
     return (
-      <div class="card">
-        <img
-          src={"http://via.placeholder.com/350x150"}
-          style={{ width: "100%" }}
-        />
-        <div class="container">
-          <h4>
-            <b>John Doe</b>
-          </h4>
-        </div>
+      <div style={styles.card}>
+        <img src={"http://via.placeholder.com/450x250"} style={styles.image} />
+        <h2 style={styles.spanTitle}>Leica</h2>
       </div>
     );
   }
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,52 @@
 import React, { Component } from "react";
 import Card from "../components/Card";
 import logo from "../images/logo.svg";
-import "./App.css";
+import injectStyle from "../utils/injectStyle";
+
+const styles = {
+  app: {},
+  applogo: {
+    animation: "App-logo-spin infinite 20s linear",
+    height: "80px"
+  },
+  appHeader: {
+    background: "black",
+    color: "white",
+    textAlign: "center",
+    height: "150px",
+    padding: "20px"
+  },
+  appTitle: { fontSize: "1.5em" },
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+};
+
+const keyframesStyle = `
+@keyframes App-logo-spin {
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+}
+`;
+
+injectStyle(keyframesStyle);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">
+      <div style={styles.app}>
+        <header style={styles.appHeader}>
+          <img src={logo} style={styles.applogo} alt="logo" />
+          <h1 style={styles.appTitle}>
             {process.env.REACT_APP_OPENDOTA != null
               ? "Dota Stats"
               : "Api Key Not Present"}
           </h1>
+          <p>A place for dota stats</p>
         </header>
-        <p className="App-intro">Default User Profile</p>
-        <div className="container">
+        <div style={styles.container}>
           <Card />
         </div>
       </div>
