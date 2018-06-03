b0c54e847c7cd07a381e67409b2c73504cc52ac9
Graham Plata <graham.plata@gmail.com>
2018-06-03 15:02:41 -0400

    move styles

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,36 +4,7 @@ import * as actionCreators from "../actions";
 import Card from "../components/Card";
 import logo from "../images/logo.svg";
 import injectStyle from "../utils/injectStyle";
-
-const styles = {
-  app: {},
-  applogo: {
-    animation: "App-logo-spin infinite 20s linear",
-    height: "80px"
-  },
-  appHeader: {
-    background: "black",
-    color: "white",
-    textAlign: "center",
-    height: "175px",
-    padding: "20px",
-    boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)"
-  },
-  appTitle: { fontSize: "1.5em" },
-  container: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-};
-
-const keyframesStyle = `
-@keyframes App-logo-spin {
-  from { transform: rotate(0deg); }
-  to { transform: rotate(360deg); }
-}
-`;
+import { styles, keyframesStyle } from "./styles";
 
 injectStyle(keyframesStyle);
 
---
+++ src/containers/styles.js
@@ -0,0 +1,29 @@
+export const styles = {
+  app: {},
+  applogo: {
+    animation: "App-logo-spin infinite 20s linear",
+    height: "80px"
+  },
+  appHeader: {
+    background: "black",
+    color: "white",
+    textAlign: "center",
+    height: "175px",
+    padding: "20px",
+    boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)"
+  },
+  appTitle: { fontSize: "1.5em" },
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+};
+
+export const keyframesStyle = `
+  @keyframes App-logo-spin {
+    from { transform: rotate(0deg); }
+    to { transform: rotate(360deg); }
+  }
+  `;
