f4f983da50f44fd4315081269a7ecd6110718598
Graham Plata <graham.plata@gmail.com>
2018-06-08 00:40:29 -0400

    add date

--- src/components/CardFooter/index.js
+++ src/components/CardFooter/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+  dd = "0" + dd;
+}
+
+if (mm < 10) {
+  mm = "0" + mm;
+}
+
+today = mm + "/" + dd + "/" + yyyy;
+
 const CardFooter = props => (
-  <p style={{ opacity: "0.6" }}>
-    <small>Stats provided by OpenDota</small>
+  <p style={{ opacity: "0.8" }}>
+    <small>Last updated {today}</small>
   </p>
 );
 
