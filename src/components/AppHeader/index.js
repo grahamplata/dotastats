2b1cf59b0d85fd551a4572c020da3d15a4c96f1d
Graham Plata <graham.plata@gmail.com>
2019-06-14 10:44:18 -0400

    fix spin

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-// import { styles } from "../../containers/styles";
 import dota from "../../static/images/dota.svg";
 
 class AppHeader extends Component {
   render() {
     return (
       <header>
-        <img src={dota} alt="logo" />
+        <img src={dota} className="Dota-logo" alt="logo" />
         <h1>
           {process.env.REACT_APP_OPENDOTA != null
             ? "Dota Stats"
