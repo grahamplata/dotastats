44651552a2ec5eb0b8874210d83d7c2c1c5363db
Graham Plata <graham.plata@gmail.com>
2019-06-14 18:44:25 -0400

    convert header to functional component

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,20 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "../../styles/containers/Header";
 import dota from "../../static/images/dota.svg";
 
-class AppHeader extends Component {
-  render() {
-    return (
-      <Header>
-        <img src={dota} className="Dota-logo" alt="logo" />
-        <h1>
-          {process.env.REACT_APP_OPENDOTA != null
-            ? "Dota Stats"
-            : "Api Key Not Present"}
-        </h1>
-      </Header>
-    );
-  }
+function AppHeader() {
+  return (
+    <Header>
+      <img src={dota} className="Dota-logo" alt="logo" />
+      <h1>Dota Stats</h1>
+    </Header>
+  );
 }
 
 export default AppHeader;
